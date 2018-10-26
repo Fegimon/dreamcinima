02a0efd5f556930d9bc692d7e76b99868e74c12a
shanvicky <shan.vicky.1990@gmail.com>
2018-10-26 18:06:08 +0530

    resources

---
+++ resources/views/admin/pages/videogallery.blade.php
@@ -0,0 +1,63 @@
+@extends('admin.default')
+@section('content')
+<style>
+.modal-dialog {width:600px;}
+.thumbnail {margin-bottom:6px;}
+</style>
+<section class="content">
+   <div class="row">
+      <div class="col-md-12">
+            <div class="box box-danger">
+                <div class="box-header">
+                     <h3 class="box-title">Video Thumb </h3>
+                </div>
+                <div class="box-body">
+                        <div class="container">
+            <div class="row">
+          
+            @foreach($images as $val)
+            <div class="col-lg-3 col-sm-3 col-xs-6">
+                 <a title="Image 1" href="#">
+                    <img class="thumbnail img-responsive" src="{{ asset('public/upload/gallery/thumbnail/'.$val->thumb_image) }}" width="150px">
+                 </a>
+            </div>
+            @endforeach
+           
+            
+        </div>
+        </div>
+        <div tabindex="-1" class="modal fade" id="myModal" role="dialog">
+        <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button class="close" type="button" data-dismiss="modal">×</button>
+                <h3 class="modal-title">Heading</h3>
+            </div>
+            <div class="modal-body">
+                
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+        </div>
+        </div>
+
+                </div>  
+            </div>
+        </div>  
+   </div>
+</section>
+
+<script>
+$(document).ready(function() {
+$('.thumbnail').click(function(){
+      $('.modal-body').empty();
+  	var title = $(this).parent('a').attr("title");
+  	$('.modal-title').html(title);
+  	$($(this).parents('div').html()).appendTo('.modal-body');
+  	$('#myModal').modal({show:true});
+});
+});
+</script>
+@stop
