4f71d4090fbdc328dc487b4759432a6acf8ca84f
anusri21 <anusrimariyappan@gmail.com>
2018-10-29 19:31:10 +0530

    db

--- database/migrations/2018_10_29_081807_create_admins_table.php
+++ database/migrations/2018_10_29_081807_create_admins_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAdminsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('email');
@@ -21,8 +21,6 @@ class CreateUsersTable extends Migration
             $table->string('dob')->nullable();
             $table->string('gender')->nullable();
             $table->string('address')->nullable();
-            $table->string('subscription')->nullable();
-            $table->date('subscribe_expiredate')->nullable();
             $table->string('password')->nullable();
             $table->tinyInteger('status')->default('1');
             $table->rememberToken();
@@ -37,6 +35,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admins');
     }
 }
