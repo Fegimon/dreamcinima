d14e662b6e54221371a2a2265ae4134776d5e098
anusri21 <anusrimariyappan@gmail.com>
2018-10-30 18:18:51 +0530

    pages

--- resources/views/admin/pages/subscriptionlist.blade.php
+++ resources/views/admin/pages/subscriptionlist.blade.php
@@ -76,6 +76,7 @@
                         </tr>
                      </thead>
                      <tbody id="list">
+                     
                        
                      </tbody>
                   </table>
