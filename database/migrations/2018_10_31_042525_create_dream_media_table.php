b878f18d9c85ea9798dca02da3c526b71e048f4f
anusri21 <anusrimariyappan@gmail.com>
2018-10-31 13:28:08 +0530

    db

--- database/migrations/2018_10_31_042525_create_dream_media_table.php
+++ database/migrations/2018_10_31_042525_create_dream_media_table.php
@@ -19,7 +19,6 @@ class CreateDreamMediaTable extends Migration
             $table->string('media_desc');
             $table->string('media_url');
             $table->string('media_thumb')->nullable();
-            $table->string('media_image')->nullable();
             $table->tinyInteger('showin_home')->default('0');
             $table->tinyInteger('status')->default('1');
             $table->timestamps();
